e443aa7a77fd35f4bc7b31795e6e467f9a58dc4f
harsha-saravanan <harshasaravanan3010@gmail.com>
2026-09-16 19:03:34 +0530

    Add mini-calculator for basic arithmetic operations

---
+++ mini-calculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"Enter A and B Values:";
+cin>>a>>b;
+cout<<"\n Addition="<<a+b;
+cout<<"\n Difference="<<a-b;
+cout<<"\n Product="<<a*b;
+cout<<"\n Division="<<a/b;
+cout<<"\n Modulus="<<a%b;
+}
