aa7b7768d82bc4934899bdf4fd6ee9370184d59b
harsha-saravanan <harshasaravanan3010@gmail.com>
2026-09-16 18:48:59 +0530

    Implement even or odd number checker

---
+++ even-or-odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"Enter 2 Numbers:";
+cin>>a>>b;
+ if(a%2==0)
+   cout<<"It is an Even Number \n";
+ else
+   cout<<"It is an Odd Number \n";
+ if(b%2==0)
+   cout<<"It is an Even Number \n";
+  else
+   cout<<"It is an Odd Number \n";
+}
