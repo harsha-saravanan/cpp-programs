f5efe85c6f7ce3abefe7d9de9c7ad334b3dbc4ec
harsha-saravanan <harshasaravanan3010@gmail.com>
2026-09-22 22:34:04 +0530

    Add if-else logic to compare two numbers

---
+++ if_else.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a,b;
+ cout<<"Enter A and B:";
+ cin>>a>>b;
+   if(a>b)
+     cout<<"A is Greater";
+   else
+     cout<<"B is Greater";
+}
